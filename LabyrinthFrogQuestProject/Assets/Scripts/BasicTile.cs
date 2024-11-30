927432fb6d8c4046d5e65cd861091346bbcc57e0
Edgar Sjöberg <110986462+EdgarSjoberg@users.noreply.github.com>
2024-11-30 16:19:39 +0100

    movement fixed :100:

--- LabyrinthFrogQuestProject/Assets/Scripts/BasicTile.cs
+++ LabyrinthFrogQuestProject/Assets/Scripts/BasicTile.cs
@@ -1,24 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class BasicTile : Tile
 {
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public override bool CheckPaths()
+    public override bool CheckPaths(int direction)
     {
-        return true;
+        if (paths[direction])
+            return true;
+        else return false;
     }
 }
