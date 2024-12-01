d0bb340c5aa308b8ec2e7e57eb1bb76190378890
Edgar Sjöberg <110986462+EdgarSjoberg@users.noreply.github.com>
2024-12-01 12:10:52 +0100

    idk what ive done

--- LabyrinthFrogQuestProject/Assets/Scripts/BasicTile.cs
+++ LabyrinthFrogQuestProject/Assets/Scripts/BasicTile.cs
@@ -1,14 +1,27 @@
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
 public class BasicTile : Tile
 {
 
     // Start is called before the first frame update
     void Start()
     {
+    }
+    private void Awake()
+    {
         if(hasScoreCollectable)
         {
             //Instantiate collectable
-            gameObject.AddComponent<ScoreCollectable>();
+            Collectable scoreCollectable = GameManager.GetScoreCollectable();
+            Collectable collectable = Instantiate(scoreCollectable);
+            collectable.transform.SetParent(this.transform);
+            collectable.transform.localPosition = new Vector3(0, 0, -1);
+            GameManager.AddScoreCollectable(collectable);
         }
+        
     }
 
     // Update is called once per frame
--- LabyrinthFrogQuestProject/Assets/Scripts/GameManager.cs
+++ LabyrinthFrogQuestProject/Assets/Scripts/GameManager.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
+ 
     [SerializeField] static int score = 0;
+    [SerializeField] Collectable scoreCollectable;
+    static Collectable ScoreCollectable;
+
+    static List<Collectable> scoreCollectables = new List<Collectable>();
     void Start()
     {
-        
+        ScoreCollectable = this.scoreCollectable;
     }
 
     // Update is called once per frame
@@ -20,5 +26,19 @@ public class GameManager : MonoBehaviour
     public static void AddScore(int value)
     {
         score += value;
+    }
+
+    public static Collectable GetScoreCollectable()
+    {
+        return ScoreCollectable;
+    }
+
+    public static Collectable GetRandomScoreCollectable()
+    {
+        return ScoreCollectable;
+    }
+    public static void AddScoreCollectable(Collectable collectable)
+    {
+        scoreCollectables.Add(collectable);
     }
 }
